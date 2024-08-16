71a176fa008e18390aed190c30b9ac11bcaa127e
Abdelkadir KEBIR <kader.kebir@gmail.com>
2024-08-16 10:34:51 +0200

    Add serilog and sink for file output

--- solution/EnergyPlanner.Api/Controllers/ProductionPlanController.cs
+++ solution/EnergyPlanner.Api/Controllers/ProductionPlanController.cs
@@ -2,9 +2,20 @@ using Microsoft.AspNetCore.Mvc;
 
 [Route("[controller]")]
 [ApiController]
-public class ProductionPlanController : ControllerBase{
+public class ProductionPlanController : ControllerBase
+{
+
+    private readonly ILogger<ProductionPlanController> _logger;
+
+    public ProductionPlanController(ILogger<ProductionPlanController> logger)
+    {
+        _logger = logger;
+    }
+
     [HttpGet]
-    public string GetProductionPlan(){
+    public string GetProductionPlan()
+    {
+        _logger.LogInformation("GetProductionPlan called");
         return "Ok";
     }
 }
--- solution/EnergyPlanner.Api/Program.cs
+++ solution/EnergyPlanner.Api/Program.cs
@@ -1,10 +1,17 @@
+using Serilog;
+
 var builder = WebApplication.CreateBuilder(args);
+
+Log.Logger = new LoggerConfiguration()
+    .WriteTo.File("logs/log.txt", rollingInterval: RollingInterval.Day)
+    .CreateLogger();
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddControllers();
 builder.Services.AddSwaggerGen();
+builder.Host.UseSerilog();
 
 var app = builder.Build();
 
